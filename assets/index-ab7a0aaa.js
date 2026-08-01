8dc530aa88aee8dfe240575e3402b0e092d0a42b
nucash-mining <nucash.mining@users.noreply.github.com>
2026-07-31 20:39:21 -0400

    Namespace the merged-mining hash: #mm/<chain> instead of bare #<chain>

--- assets/index-ab7a0aaa.js
+++
@@ -1,2 +0,0 @@
-import{_ as p}from"./index-e7595581.js";import{registerPlugin as r}from"./index-11572db4.js";const _=r("App",{web:()=>p(()=>import("./web-680e68c5.js"),["assets/web-680e68c5.js","assets/index-11572db4.js"]).then(e=>new e.AppWeb)});export{_ as App};
-//# sourceMappingURL=index-ab7a0aaa.js.map

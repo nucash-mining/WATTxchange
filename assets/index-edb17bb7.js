4ea78aa7e9c99872a1478c374a31afccfb82168d
nucash-mining <nucash.mining@users.noreply.github.com>
2026-08-01 07:56:10 -0400

    Deploy: Explorer tab points at the live WATTx explorer

--- assets/index-edb17bb7.js
+++
@@ -1,2 +0,0 @@
-import{_ as p}from"./index-b14720b0.js";import{registerPlugin as r}from"./index-11572db4.js";const _=r("App",{web:()=>p(()=>import("./web-680e68c5.js"),["assets/web-680e68c5.js","assets/index-11572db4.js"]).then(e=>new e.AppWeb)});export{_ as App};
-//# sourceMappingURL=index-edb17bb7.js.map

0db2ab2ce82afcd45fefaae06e2cc8c8bb0e6697
nucash-mining <nucash-mining@users.noreply.github.com>
2026-08-10 01:06:23 -0500

    deploy: auth modal portal fix

--- assets/index-003609c1.js
+++
@@ -1,2 +0,0 @@
-import{_ as p}from"./index-a8b11be8.js";import{registerPlugin as r}from"./index-11572db4.js";const _=r("App",{web:()=>p(()=>import("./web-680e68c5.js"),["assets/web-680e68c5.js","assets/index-11572db4.js"]).then(e=>new e.AppWeb)});export{_ as App};
-//# sourceMappingURL=index-003609c1.js.map

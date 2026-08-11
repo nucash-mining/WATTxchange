84272fe29074e9f48b951b281c84c0aae1f64e0d
nucash-mining <nucash.mining@gmail.com>
2026-08-10 20:39:39 -0500

    Bridge UI: show 'deposit detected — N/3 confirmations' while a WTX deposit confirms

--- assets/index-ae03b3f9.js
+++
@@ -1,2 +0,0 @@
-import{_ as p}from"./index-70549a3c.js";import{registerPlugin as r}from"./index-11572db4.js";const _=r("App",{web:()=>p(()=>import("./web-680e68c5.js"),["assets/web-680e68c5.js","assets/index-11572db4.js"]).then(e=>new e.AppWeb)});export{_ as App};
-//# sourceMappingURL=index-ae03b3f9.js.map

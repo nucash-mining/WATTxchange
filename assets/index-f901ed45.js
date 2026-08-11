014440f9b64f5087d8d9a98ca71313a723a83e80
nucash-mining <nucash.mining@gmail.com>
2026-08-11 01:17:05 -0500

    DEX: CONNECT button opens sign-in when logged out (bypasses injected-wallet masking of the seed sign-in)

---
+++ assets/index-f901ed45.js
@@ -0,0 +1,2 @@
+import{_ as p}from"./index-62c4edbe.js";import{registerPlugin as r}from"./index-11572db4.js";const _=r("App",{web:()=>p(()=>import("./web-680e68c5.js"),["assets/web-680e68c5.js","assets/index-11572db4.js"]).then(e=>new e.AppWeb)});export{_ as App};
+//# sourceMappingURL=index-f901ed45.js.map

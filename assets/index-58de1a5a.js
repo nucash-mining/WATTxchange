736641367a74c9f440c1d317b15374c8ba0ff2d6
nucash-mining <nucash.mining@gmail.com>
2026-08-12 19:53:15 -0500

    GHOST: point DEX electrum to Legion WSS (electrum-ghost.wattxchange.app:443), mark electrumReady

---
+++ assets/index-58de1a5a.js
@@ -0,0 +1,2 @@
+import{_ as p}from"./index-3a286f7f.js";import{registerPlugin as r}from"./index-11572db4.js";const _=r("App",{web:()=>p(()=>import("./web-680e68c5.js"),["assets/web-680e68c5.js","assets/index-11572db4.js"]).then(e=>new e.AppWeb)});export{_ as App};
+//# sourceMappingURL=index-58de1a5a.js.map
